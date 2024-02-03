557a95b7a3e4664258d6ccfcf9ba06f9f49da785
IUGAI1398 <hhhs2332@gmail.com>
2024-02-03 14:22:51 +0900

    re init project changed gitignore

---
+++ hydrogen_infra_api/src/test/java/com/hydrogen_infra_api/hydrogen_infra_api/HydrogenInfraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydrogen_infra_api.hydrogen_infra_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydrogenInfraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
